634193baee60c0b8ceb641bd21ad9d6eeab2d637
Stephen Pullen <kurropike@gmail.com>
2020-06-01 06:50:21 -0600

    Challenge 2

--- challenge2.js
+++ challenge2.js
@@ -20,24 +20,17 @@ describe("challenge2gi suite", function(){
     it("Should open the copart website", function() {
         return driver.get("http://www.copart.com");
     });
-    it("Should run a search for 'exotic'", function() {
-        return driver.findElement(By.xpath('//form[@id="search-form"]//input')).sendKeys('exotic' + Key.ENTER)
-    });
-    // it("should look at serverSideDataTable to find PORSCHE in the list", async function() {
-    //     await driver.wait(until.titleContains('exotic'), 10000);
-    //     console.log(await driver.getTitle());
-    //     var html = await driver.findElement(By.id(''))
 
-    // });
-    
-    it("The title is '100% Online Auto Auction - Copart USA - Salvage Cars for Sale in Online Car Auctions'", function() {
-        return driver.getTitle().then(function(title) {
-            assert.equal(title, "100% Online Auto Auction - Copart USA - Salvage Cars for Sale in Online Car Auctions");
-        });
+    it("Should run a search for 'exotics'", async function() {
+        var element = await driver.findElement(By.xpath('//form[@id="search-form"]//input'));
+        return element.sendKeys("exotics" + Key.ENTER)
     });
-    it("The title is '100% Online Auto Auction - Copart USA - Salvage Cars for Sale in Online Car Auctions'", async function() {
-        var title = await driver.getTitle();
-        return assert.include(title, "100% Online Auto Auction - Copart USA - ");
+    it("should assert that PORSCHE is in list of results", async function() {
+        await driver.wait(until.titleContains('exotics'), 10000);
+        console.log(await driver.getTitle());
+        await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//table[@id="serverSideDataTable"]'))))
+        var html = await driver.findElement(By.id("serverSideDataTable")).getAttribute('innerHTML');
+        return assert.include(html, "PORSCHE")
     });
  
  
